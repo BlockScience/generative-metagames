7727104a10754a4063bb96e69d4985ae1bc3814c
Michael Zargham <mzargham@users.noreply.github.com>
2021-12-19 13:55:21 -0500

    param sweep

--- killer-tree/model/params.py
+++ killer-tree/model/params.py
@@ -2,6 +2,6 @@ params = {
     "alice_payoffs" : [{'effort': -1, 'view':2}],
     "bob_payoffs" : [{'cost':-1, 'tree':2} ],
     "prior_tree_sick": [.001],
-    "poison_stregth": [.5],
-    "doctor_skill": [.5]
+    "poison_stregth": [.9],
+    "doctor_skill": [.1, .5, .9]
     }
